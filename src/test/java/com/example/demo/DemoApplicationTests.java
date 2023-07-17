4d253da33acb9fa44bf0f31b3ec078e16e90d1a8
Pandi Sundaram <pandi.sundaram@anywhere.co>
2023-07-17 10:44:28 +0530

    rc

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,28 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class DemoApplicationTests {
+
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void testSize() {
+		List<String> obj = new ArrayList<>();
+		obj.add("Emma");
+		obj.add("Ronan");
+		obj.add("Antonio");
+		assertEquals("Checking size of List", 3, obj.size());
+	}
+
+
 
 }
